a76a3de4592f7fff868e478a59f7b36e8e283d23
Zahran <zahranvidian@gmail,com>
2023-07-12 16:06:34 +0700

    done

---
+++ app/src/main/java/com/dicoding/habitapp/data/HabitRepository.kt
@@ -0,0 +1,68 @@
+package com.dicoding.habitapp.data
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.dicoding.habitapp.utils.HabitSortType
+import com.dicoding.habitapp.utils.SortUtils
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class HabitRepository(private val habitDao: HabitDao, private val executor: ExecutorService) {
+
+    companion object {
+
+        @Volatile
+        private var instance: HabitRepository? = null
+
+        fun getInstance(context: Context): HabitRepository {
+            return instance ?: synchronized(this) {
+                if (instance == null) {
+                    val database = HabitDatabase.getInstance(context)
+                    instance = HabitRepository(
+                        database.habitDao(),
+                        Executors.newSingleThreadExecutor()
+                    )
+                }
+                return instance as HabitRepository
+            }
+
+        }
+    }
+
+    //TODO 4 : Use SortUtils.getSortedQuery to create sortable query and build paged list
+    fun getHabits(filter: HabitSortType): LiveData<PagedList<Habit>> {
+        val sortQuery = SortUtils.getSorteredQuery(filter)
+        val habits = habitDao.getHabits(sortQuery)
+        val config = PagedList.Config.Builder()
+            .setPageSize(30)
+            .setEnablePlaceholders(true)
+            .setInitialLoadSizeHint(30)
+            .build()
+
+        return LivePagedListBuilder(habits,config).build()
+    }
+
+    //TODO 5 : Complete other function inside repository
+    fun getHabitById(habitId: Int): LiveData<Habit> {
+        return habitDao.getHabitById(habitId)
+    }
+
+    fun insertHabit(newHabit: Habit): Long {
+        val newHabitData = Callable { habitDao.insertHabit(newHabit)}
+        val executor = executor.submit(newHabitData)
+        return executor.get()
+    }
+
+    fun deleteHabit(habit: Habit) {
+        executor.execute {
+            habitDao.deleteHabit(habit)
+        }
+    }
+
+    fun getRandomHabitByPriorityLevel(level: String): LiveData<Habit> {
+        return habitDao.getRandomHabitByPriorityLevel(level)
+    }
+}
